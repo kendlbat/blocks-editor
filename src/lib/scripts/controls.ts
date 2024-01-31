092aebf34cb2c3589c8bb598e230ebad3906b8d5
kendlbat <kendlbat@edu.htl-villach.at>
2024-01-31 21:37:27 +0100

    If you ignore the errors, they don't exist

--- src/lib/scripts/controls.ts
+++ src/lib/scripts/controls.ts
@@ -20,14 +20,22 @@ export const controls: {
             destroy: () => void;
         }>,
         custom?: Record<string, any>,
-    ) => SvelteComponent<{ p?: any; id?: string; destroy: () => void }>;
+    ) => SvelteComponent<{ p?: any; id?: string; destroy?: () => void }>;
 } = {
+    // @ts-ignore
     declare: (options) => new Declare(options),
+    // @ts-ignore
     input: (options) => new Input(options),
+    // @ts-ignore
     output: (options) => new Output(options),
+    // @ts-ignore
     assign: (options) => new Assignment(options),
+    // @ts-ignore
     if: (options) => new If(options),
+    // @ts-ignore
     switch: (options) => new Switch(options),
+    // @ts-ignore
     while: (options) => new While(options),
+    // @ts-ignore
     dowhile: (options) => new DoWhile(options),
 };
